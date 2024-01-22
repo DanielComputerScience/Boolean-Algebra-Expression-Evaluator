49c60723c93f60752f8521453cffa8cf3338701d
DanielComputerScience <elijah.williams@student.allenisd.org>
2024-01-22 08:39:34 -0600

    updated code to add gaurd statement to check for consistent spacing

--- error.swift
+++ error.swift
@@ -1,32 +1,44 @@
-/*import Foundation
+import Foundation
 
 enum Lexeme {
     enum LexemeError : Error {
-        case failedToFindLexeme(inLine: String) 
+        case failedToFindLexeme(inLine: String)
+        inconsistentSpacingForOperator(operator: String, inLine: String)
     }
     case variableDeclaration
     case operatorAssignment
     case identifier(name: String)
     case literalInteger(value: Int)
 
-    static func  nextLexeme(from line: Substring) throws  -> Lexeme{
+    static func  nextLexeme(from line: Substring) throws  {
+        // testing for var
         if let match = try /\s*var\s*/.prefixMatch(in:line) {
-  /*          print("Found keyword var: \(match.0)")
+            print("Found keyword var: \(match.0)")
             return .variableDeclaration
+            //testing for = sign
         } else if let match = try /\s*=\s*/.prefixMatch(in: line) {
-    /*        print("Found assignment operator \(match.0)")
+            print("Found assignment operator \(match.0)")
             return .operatorAssignment
+        } else if let _ = try /(\s*)=(\s*)/.prefixMatch(in: line) {
+            guard (match.1.count == 0 && mach.2.count == 0)  ||
+                    (match.1.count > 0 && match.2.count > 0) else {
+            throw LexemeError.inconsistentSpacingForOperator(operator: "=" inLine: String(line))
+           
+            // testing for and sign
         } else if let match = try /∧/.prefixMatch(in: line) {
             print("Found assignment operator \(match.0)")
             return .operatorAssignment
+            // testing for or sign
         } else if let match = try /∨/.prefixMatch(in: line) {
             print("Found assignment operator \(match.0)")
             return .operatorAssignment
+            // test for minus sign
         } else if let match = try /-/.prefixMatch(in: line) {
             print("Found assignment operator \(match.0)")
             return .operatorAssignment
+            // 
         } else if let match = try /\s*([a-z])+\s*/.prefixMatch(in: line) {
-     /*       print("Found identifier \(match.1)")
+            print("Found identifier \(match.1)")
             return .identifier
 
         } else {
@@ -48,4 +60,3 @@ func main() {
     }
 }
 main()
-*/
